34a0bf9f60abc5dc1eafcccc1d0b642a0a041465
EvgeniyaRaym <avariyanik@mail.ru>
2023-06-12 10:32:13 +0300

    Commit

---
+++ sem 6 hw/41/Program.cs
@@ -0,0 +1,33 @@
+﻿// See Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Этот код посчитает, сколько чисел больше 0 во введенном Вами массиве. Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+Console.WriteLine($"В вашем массиве {PosSum(baseArray)} чисел больше 0");
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int PosSum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        result++;
+    } 
+    return result;
+}
---
+++ sem 6 hw/43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Данный код покажет, пересекаются ли прямые, заданные уравнениями.");
+double b1 = Imput("Введите b1: ");
+double k1 = Imput("Введите k1: ");
+double b2 = Imput("Введите b2: ");
+double k2 = Imput("Введите k2: ");
+if (k1-k2 == 0) Console.WriteLine("Пересечение невозможно");
+else
+Console.WriteLine($"Координаты точки пересечения: {Intersection(b1,k1,b2,k2)}");
+
+double[] Intersection(double b1, double k1, double b2, double k2)
+{
+    double[] res = new double[2];
+    res[0] = Convert.ToDouble((b2-b1)/(k1-k2));
+    res [1] = Convert.ToDouble((k2*b1 + b2*k1)/(k1-k2));
+    return res;
+}
+
+
+double Imput(string smth)
+{
+    Console.WriteLine(smth);
+    double res = int.Parse(Console.ReadLine());
+    return res;
+}
